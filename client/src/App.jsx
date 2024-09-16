ef615d5d60fb5c3e11e13e2635e40923a9e863ab
Nikhil Kumar <nknikhilkr73@gmail.com>
2024-09-16 13:51:15 +0530

    Created the pages an routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import NavBar from './pages/NavBar'
 
 const App = () => {
   return (
-    <div className='text-3xl text-red-500'>App</div>
+    <>  <BrowserRouter >
+      <Routes>
+        <Route path="/" element = {<Home/>}></Route>
+        <Route path="/about" element = {<About/>}></Route>
+        <Route path="/sign-up" element = {<SignUp/>}></Route>
+        <Route path="/sign-in" element = {<SignIn/>}></Route>
+        <Route path="/dashboard" element = {<Dashboard/>}></Route>
+        <Route path="/projects" element = {<Projects/>}></Route>
+      </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
