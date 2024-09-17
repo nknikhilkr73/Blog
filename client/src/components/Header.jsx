009574e703041958bb25f075309f4d2fd93161aa
Nikhil Kumar <nknikhilkr73@gmail.com>
2024-09-17 10:14:54 +0530

    Created the server and made changes in the script in package.json(server side) to run the server at the time of deployment.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,7 +30,7 @@ const path = useLocation().pathname;
       <FaMoon />  
     </Button>
     <Link to='/sign-in'>
-        <Button gradientDuoTone='purpleToBlue'>
+        <Button gradientDuoTone='purpleToBlue' outline>
 Sign In
         </Button>
     </Link>
