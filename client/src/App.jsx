01d241373e0d0337d8ac9a951d0929a26bc8a0de
Nikhil Kumar <nknikhilkr73@gmail.com>
2024-09-17 09:59:23 +0530

    Created the header Sction

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,13 @@ import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
-import NavBar from './pages/NavBar'
+import Header from './components/Header'
+
 
 const App = () => {
   return (
     <>  <BrowserRouter >
+    <Header/>
       <Routes>
         <Route path="/" element = {<Home/>}></Route>
         <Route path="/about" element = {<About/>}></Route>
